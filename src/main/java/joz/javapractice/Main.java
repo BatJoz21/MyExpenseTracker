ad1954bcd9a867af2583fabc4e82084e9b4f85a2
BatJoz21 <jozkasiregar@gmail.com>
2025-05-22 10:48:47 +0700

    Update Main.java

--- src/main/java/joz/javapractice/Main.java
+++ src/main/java/joz/javapractice/Main.java
@@ -2,15 +2,19 @@ package joz.javapractice;
 
 import joz.javapractice.model.Expense;
 import joz.javapractice.utils.ExpenseDataLoader;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
 @SpringBootApplication
 public class Main implements CommandLineRunner {
     public static void main(String[] args) {
+        String port = System.getenv("PORT");
+        System.out.println("PORT env var: " + port);
         SpringApplication.run(Main.class, args);
     }
 
@@ -19,5 +23,10 @@ public class Main implements CommandLineRunner {
         System.out.println("THE APP IS RUNNING!");
 //        List<Expense> theExpenseList = ExpenseDataLoader.getExpenses();
 //        theExpenseList.forEach(System.out::println);
+    }
+
+    @Bean
+    public CommandLineRunner logPort(@Value("${server.port}") String port) {
+        return args -> System.out.println("Spring Boot listening on port: " + port);
     }
 }
